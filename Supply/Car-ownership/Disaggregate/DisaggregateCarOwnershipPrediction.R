ff80e00af4121b994060aaf87e455c828c210586
Santhanakrishnan Narayanan <36446409+nsanthanakrishnan@users.noreply.github.com>
2021-04-15 20:43:18 +0200

    Add files via upload

--- Supply/Car-ownership/Disaggregate/DisaggregateCarOwnershipPrediction.R
+++ Supply/Car-ownership/Disaggregate/DisaggregateCarOwnershipPrediction.R
@@ -11,16 +11,16 @@
 ##READING INPUT##
 #############################################################################################################
 #Change the separator (sep) and decimal (dec) format according to input data 
-InputData <- read.csv2("SyntheticPopulation.csv", sep = ',', dec = ".") #Socio-demographics data
-CoefficientValues <- read.csv2("CoefficientValues.csv", sep = ',', dec = ".", header = FALSE) #Coefficient values
+InputData = read.csv2("SyntheticPopulation.csv", sep = ',', dec = ".") #Socio-demographics data
+CoefficientValues = read.csv2("CoefficientValues.csv", sep = ',', dec = ".", header = FALSE) #Coefficient values
 
 #############################################################################################################
 ##PREDICTION##
 #############################################################################################################
-SharesPrediction <- function(InputData) {
-  Utility <- data.frame(matrix(NA, nrow = nrow(InputData), ncol = 3))
-  colnames(Utility) <- c("OneCar","TwoCars","ThreeOrMoreCars")
-  Utility$OneCar <- CoefficientValues[1,2] +
+SharesPrediction = function(InputData) {
+  Utility = data.frame(matrix(NA, nrow = nrow(InputData), ncol = 3))
+  colnames(Utility) = c("OneCar","TwoCars","ThreeOrMoreCars")
+  Utility$OneCar = CoefficientValues[1,2] +
     CoefficientValues[4,2]*InputData$NativeCitizen +
     CoefficientValues[7,2]*InputData$LowIncome +
     CoefficientValues[10,2]*InputData$MediumIncome +
@@ -35,7 +35,7 @@ SharesPrediction <- function(InputData) {
     CoefficientValues[37,2]*InputData$CommuteSpeed
   
   
-  Utility$TwoCars <- CoefficientValues[2,2] +
+  Utility$TwoCars = CoefficientValues[2,2] +
     CoefficientValues[5,2]*InputData$NativeCitizen +
     CoefficientValues[8,2]*InputData$LowIncome +
     CoefficientValues[11,2]*InputData$MediumIncome +
@@ -50,7 +50,7 @@ SharesPrediction <- function(InputData) {
     CoefficientValues[38,2]*InputData$CommuteSpeed
   
   
-  Utility$ThreeOrMoreCars <- CoefficientValues[3,2] +
+  Utility$ThreeOrMoreCars = CoefficientValues[3,2] +
     CoefficientValues[6,2]*InputData$NativeCitizen +
     CoefficientValues[9,2]*InputData$LowIncome +
     CoefficientValues[12,2]*InputData$MediumIncome +
@@ -65,24 +65,25 @@ SharesPrediction <- function(InputData) {
     CoefficientValues[39,2]*InputData$CommuteSpeed
   
   #Prediction
-  Utility$expOneCar <- exp(Utility$OneCar)
-  Utility$expTwoCars <- exp(Utility$TwoCars)
-  Utility$expThreeOrMoreCars <- exp(Utility$ThreeOrMoreCars)
-  Utility$TotalUtility <- rowSums(Utility[,4:6])
-  Utility$TotalUtility <- Utility$TotalUtility + exp(0)
+  Utility$expOneCar = exp(Utility$OneCar)
+  Utility$expTwoCars = exp(Utility$TwoCars)
+  Utility$expThreeOrMoreCars = exp(Utility$ThreeOrMoreCars)
+  Utility$TotalUtility = rowSums(Utility[,4:6])
+  Utility$TotalUtility = Utility$TotalUtility + exp(0)
   
-  Utility$probOneCar <- Utility$expOneCar/Utility$TotalUtility
-  Utility$probTwoCars <- Utility$expTwoCars/Utility$TotalUtility
-  Utility$probThreeOrMoreCars <- Utility$expThreeOrMoreCars/Utility$TotalUtility
-  Utility$probNoCar <- exp(0)/Utility$TotalUtility
+  Utility$probOneCar = Utility$expOneCar/Utility$TotalUtility
+  Utility$probTwoCars = Utility$expTwoCars/Utility$TotalUtility
+  Utility$probThreeOrMoreCars = Utility$expThreeOrMoreCars/Utility$TotalUtility
+  Utility$probNoCar = exp(0)/Utility$TotalUtility
   Utility$totalProb = rowSums(Utility[,8:11])
   
-  shares <- colMeans(Utility[,8:11]) #, na.rm = TRUE
+  shares = colMeans(Utility[,8:11]) #, na.rm = TRUE
   return(Utility[,c(11, 8:10)])
 }
 
-PredictedShares <- SharesPrediction(InputData)
-InputDataWithCarOwnership[(ncol(InputDataWithCarOwnership)+1):(ncol(InputDataWithCarOwnership)+4)] <- PredictedShares
+PredictedShares = SharesPrediction(InputData)
+InputDataWithCarOwnership = InputData
+InputDataWithCarOwnership[(ncol(InputDataWithCarOwnership)+1):(ncol(InputDataWithCarOwnership)+4)] = PredictedShares
 InputDataWithCarOwnership$CarOwnership = max.col(PredictedShares, "first") - 1
 
 #############################################################################################################
