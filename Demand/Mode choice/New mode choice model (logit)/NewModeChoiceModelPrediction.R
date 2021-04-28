b8d1352276a253ba2b502f678614b9237ce9316c
Santhanakrishnan Narayanan <36446409+nsanthanakrishnan@users.noreply.github.com>
2021-04-28 15:27:14 +0200

    Add files via upload

--- Demand/Mode choice/New mode choice model (logit)/NewModeChoiceModelPrediction.R
+++ Demand/Mode choice/New mode choice model (logit)/NewModeChoiceModelPrediction.R
@@ -1,7 +1,7 @@
 ############################################################################################################
 ##SCRIPT DETAILS##
 ############################################################################################################
-#Input: Synthetic population
+#Input: Synthetic population, travel times, sharing vehicle availability, bike-sharing supply (in hundreds)
 #Output: Mode choice
 #Type of model: Multinomial logit model
 #The coefficients are read from the file 'coefficients.csv'. This file is provided to allow custom values for the coefficients
@@ -14,6 +14,31 @@
 InputData <- read.csv2("InputSampleFile.csv", sep = ',', dec = ".") 
 CoefficientValues <- read.csv2("CoefficientValues.csv", sep = ',', dec = ".", header = FALSE) #Coefficient values
 
+#############################################################################################################
+##DATA TRANSFORMATION##
+#############################################################################################################
+InputData$Age20To44 = ifelse(InputData$Age>=20 & InputData$Age<=44,1,0)
+InputData$Male = ifelse(InputData$Gender=="Male",1,0)
+InputData$UniversityOrVocationalDegree = ifelse(InputData$Education == "University" | InputData$Education == "Vocational",1,0)
+
+InputData$TripDistKM2 = ifelse(InputData$TripDistanceKM <= 2,1,0) 
+InputData$TripDistKM2To5 = ifelse(InputData$TripDistanceKM > 2 & InputData$TripDistanceKM <= 5,1,0)
+InputData$TripDistKM5To15 = ifelse(InputData$TripDistanceKM > 5 & InputData$TripDistanceKM <= 15,1,0)
+
+InputData$TravelTimeMins30_Bikesharing = ifelse(InputData$TravelTimeMins_Bikesharing <= 30,1,0)
+InputData$TravelTimeMins15_Carsharing = ifelse(InputData$TravelTimeMins_Carsharing <= 15,1,0)
+InputData$TravelTimeMins15To30_Carsharing = ifelse(InputData$TravelTimeMins_Carsharing > 15 & InputData$TravelTimeMins_Carsharing <= 30,1,0)
+InputData$TravelTimeMins15_Ridesharing = ifelse(InputData$TravelTimeMins_Ridesharing <= 15,1,0)
+InputData$TravelTimeMins15To30_Ridesharing = ifelse(InputData$TravelTimeMins_Ridesharing > 15 & InputData$TravelTimeMins_Ridesharing <= 30,1,0)
+
+InputData$NoBLicense = 1
+InputData$NoBLicense[InputData$LicenseType=="B" | InputData$LicenseType=="A,B"] = 0
+InputData$BikesharingSupply = InputData$BikesharingSupply/100
+
+InputData$VehicleNonAvailability_Bikesharing = ifelse(InputData$VehicleAvailability_Bikesharing==0,1,0)
+InputData$VehicleNonAvailability_Carsharing = ifelse(InputData$VehicleAvailability_Carsharing==0,1,0)
+InputData$VehicleNonAvailability_Ridesharing = ifelse(InputData$VehicleAvailability_Ridesharing==0,1,0)
+  
 #############################################################################################################
 ##PREDICTION##
 #############################################################################################################
@@ -28,9 +53,9 @@ SharesPrediction <- function(InputData) {
     CoefficientValues[17,2]*InputData$HHCars +
     CoefficientValues[19,2]*InputData$TripDistKM2 +
     CoefficientValues[20,2]*InputData$TripDistKM2To5 +
-    CoefficientValues[29,2]*InputData$TravelTimeMins30 +
+    CoefficientValues[29,2]*InputData$TravelTimeMins30_Bikesharing +
     CoefficientValues[30,2]*InputData$BikesharingSupply +
-    CoefficientValues[31,2]*InputData$VehicleNonAvailabilityBikesharing
+    CoefficientValues[31,2]*InputData$VehicleNonAvailability_Bikesharing
     
   
   Utility$Carsharing <- CoefficientValues[2,2] +
@@ -42,9 +67,9 @@ SharesPrediction <- function(InputData) {
     CoefficientValues[18,2]*InputData$HHCars +
     CoefficientValues[21,2]*InputData$TripDistKM2To5 +
     CoefficientValues[23,2]*InputData$TripDistKM5To15 +
-    CoefficientValues[25,2]*InputData$TravelTimeMins15 +
-    CoefficientValues[27,2]*InputData$TravelTimeMins15To30 +
-    CoefficientValues[32,2]*InputData$VehicleNonAvailabilityCarsharing
+    CoefficientValues[25,2]*InputData$TravelTimeMins15_Carsharing +
+    CoefficientValues[27,2]*InputData$TravelTimeMins15To30_Carsharing +
+    CoefficientValues[32,2]*InputData$VehicleNonAvailability_Carsharing
   
   Utility$Ridesharing <- CoefficientValues[3,2] +
     CoefficientValues[6,2]*InputData$Age20To44 +
@@ -53,9 +78,9 @@ SharesPrediction <- function(InputData) {
     CoefficientValues[16,2]*InputData$PTPass +
     CoefficientValues[22,2]*InputData$TripDistKM2To5 +
     CoefficientValues[24,2]*InputData$TripDistKM5To15 +
-    CoefficientValues[26,2]*InputData$TravelTimeMins15 +
-    CoefficientValues[28,2]*InputData$TravelTimeMins15To30 +
-    CoefficientValues[33,2]*InputData$VehicleNonAvailabilityRidesharing
+    CoefficientValues[26,2]*InputData$TravelTimeMins15_Ridesharing +
+    CoefficientValues[28,2]*InputData$TravelTimeMins15To30_Ridesharing +
+    CoefficientValues[33,2]*InputData$VehicleNonAvailability_Ridesharing
   
   #Prediction
   Utility$expBikesharing <- exp(Utility$Bikesharing)
